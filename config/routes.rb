7e9b30e38c65c84bccf1ad9043dc13700101521b
Tyler Valley <tvalley93@gmail.com>
2019-02-11 18:20:17 -0500

    add user and stock association plus views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'users#my_portfolio'
   get 'welcome/index', to: 'welcome#index'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
+
 end
