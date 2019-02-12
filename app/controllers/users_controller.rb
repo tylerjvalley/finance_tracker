d9d82f9c8ab8ff1cc530a095c958a7ef38a18295
Tyler Valley <tvalley93@gmail.com>
2019-02-12 16:41:35 -0500

    Add search users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
   def my_friends
     @friendships = current_user.friends
   end
+
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You didn't enter anything..."
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "No users match search criteria" if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
+  def add_friend
+
+  end
 end
