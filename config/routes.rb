fc3db646bb95581c4ba5c14be8200ba779da35d0
Tyler Valley <tvalley93@gmail.com>
2019-02-12 15:32:08 -0500

    Add friends page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'welcome/index', to: 'welcome#index'
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_friends', to: 'users#my_friends'
 
 end
